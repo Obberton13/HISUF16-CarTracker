bd7e6a85e3a3a2cb86743ca89f0d1c13cc06a726
mrmerritth <mrmerritth@yahoo.com>
2016-09-17 13:27:29 -0500

    drop/clear a pin
    drops a pin in ames and removes it via button

--- app/src/main/java/com/example/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -1,14 +1,18 @@
 package com.example.myapplication;
 
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -26,7 +30,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     /**
-     * Manipulates the map once available.
+     * Manipulates the map once available.java.lang.IllegalStateException: MyLocation layer not enabled
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
      * we just add a marker near Sydney, Australia.
@@ -37,10 +41,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        Location loc;
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng ames = new LatLng(42.0308, -93.6319);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ames));
+
+
+
+        /*loc = mMap.getMyLocation();
+        LatLng curloc = new LatLng(loc.getLatitude(), loc.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(curloc).title("Your car is here."));*/
     }
+
+    public void dropPin(View view) {
+        LatLng ames = new LatLng(42.0308, -93.6319);
+        mMap.addMarker(new MarkerOptions().position(ames).title("Your car is here."));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ames));
+
+    }
+    public void clearPins(View view){
+        mMap.clear();
+    }
+
 }
